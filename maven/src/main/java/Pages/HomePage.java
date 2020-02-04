80dcf6b94f72ca59e6069b8681c56729ed8d9c0d
Dev <ramkumaar22593@gmail.com>
2020-02-04 15:41:49 +0530

    NewCommint Man

---
+++ maven/src/main/java/Pages/HomePage.java
@@ -0,0 +1,42 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends LoginPage {
+	
+	
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}	
+	
+	@FindBy(how=How.ID,using="addproject")
+	private WebElement clickproject;
+	public HomePage createproject() throws InterruptedException{
+		Thread.sleep(1000);
+		clickproject.click();
+		return this;	
+	}
+	
+	@FindBy(how=How.ID,using="projectname")
+	private WebElement typeName;
+	public HomePage typename(){
+		typeName.sendKeys("MassssProject");
+		takeSnap();
+		System.out.println("passed");
+		return this;	
+	}
+	
+	@FindBy(how=How.ID,using="createproject")
+	private WebElement createProject;
+	public HomePage project(){
+		createProject.click();
+		//takeSnap();
+		//switchToWindow(1);
+		return new AddSPrintandTask();	
+		
+	}
+
+}
